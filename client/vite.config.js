6044d47d477d0c5e3f0780c07a80879f960223b1
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-06-04 13:28:59 +0530

    vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Fixed the package name ordering here!
-import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 })
